f91613ee7f31730a352f9673764f897299a6b846
Andrew Williams <1williamsaj@gmail.com>
2019-09-10 01:44:12 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,7 +15,9 @@ def third_challenge
           :age => "15",
           :status => "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        
+      ]
    },
 
    :capulet => {
